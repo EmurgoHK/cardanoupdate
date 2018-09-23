a89a3e68915881a65418c1e9dc1e1457a2ec27c3
Madan Bhandari <madan.kbh@gmail.com>
2018-09-23 21:32:24 +0530

    Problem : News list UX is not good #12
    Solution : Tabel replaced with cards and ux updated

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -11,55 +11,69 @@ import { removeNews } from '/imports/api/news/methods'
 
 import swal from 'sweetalert2'
 
-Template.home.onCreated(function() {
-    this.autorun(() => {
-        this.subscribe('news')
-        this.subscribe('users')
-
-        this.subscribe('comments')
-    })
+Template.home.onCreated(function () {
+  this.autorun(() => {
+    this.subscribe('news')
+    this.subscribe('users')
+    this.subscribe('comments')
+  })
 })
 
 Template.home.helpers({
-    news: () => News.find({}),
-    author: function() {
-        return (Meteor.users.findOne({
-            _id: this.createdBy
-        }) || {}).username || ''
-    },
-    comments: function() {
-        return Comments.find({
-            parentId: this._id
-        }).count()
-    },
-    canEdit: function() {
-        return this.createdBy === Meteor.userId()
+  newsCount(){
+    let news =  News.find({}).count()
+    if(news){
+      return true
     }
+    return false
+  },
+  news: function(){
+    let news =  News.find({})
+    return news.map(a => {
+      let user = Meteor.users.findOne({_id : a.createdBy})
+      let canEdit = (Meteor.userId() === a.createdBy) ? true : false
+      return {
+        newsId : a._id,
+        // TODO : Update this user Name
+        author : user._id,
+        headline : a.headline,
+        summary : a.summary,
+        slug : a.slug,
+        date : new Date(a.createdAt).toLocaleString(),
+        canEdit
+      }
+    })
+  },
+  comments: function () {
+    return Comments.find({
+      parentId: this._id
+    }).count()
+  },
 })
 
 Template.home.events({
-    'click #js-new': (event, templateInstance) => {
-        event.preventDefault()
+  'click #js-new': (event, templateInstance) => {
+    event.preventDefault()
 
-        FlowRouter.go('/add')
-    },
-    'click #js-remove': function(event, templateInstance) {
-        event.preventDefault()
+    FlowRouter.go('/add')
+  },
+  'click #js-remove': function (event, templateInstance) {
+    event.preventDefault()
 
-        swal({
-            text: `Are you sure you want to remove this news item? This action is not reversible.`,
-            type: 'warning',
-            showCancelButton: true
-        }).then(confirmed => {
-            if (confirmed.value) {
-                removeNews.call({
-                    newsId: this._id
-                }, (err, data) => {
-                    if (err) {
-                        notify(err.reason || err.message, 'error')
-                    }
-                })
-            }
+    swal({
+      text: `Are you sure you want to remove this news item? This action is not reversible.`,
+      type: 'warning',
+      showCancelButton: true
+    }).then(confirmed => {
+      if (confirmed.value) {
+        removeNews.call({
+          newsId: this.newsId
+        }, (err, data) => {
+          if (err) {
+            notify(err.reason || err.message, 'error')
+          }
         })
-    }
+      }
+    })
+  }
 })
