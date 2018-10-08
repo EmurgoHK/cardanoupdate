d6abe2158886f76d2f9f05a5a8ce599082721826
Dilpreet Singh <singhsince94@gmail.com>
2018-10-08 16:35:12 +0530

    problem: some info is missing from project detail page #101
    solution: Adds github & website links to the details page

--- imports/ui/pages/projects/viewProject.js
+++ imports/ui/pages/projects/viewProject.js
@@ -136,5 +136,70 @@ Template.viewProject.events({
 				templateInstance[`${cool ? 'cool' : 'flag'}Message`].set(err.reason || err.message)
 			}
 		})
-	}
+	},
+	'click .github': function(event, temlateInstance) {
+        if ($(event.currentTarget).attr('href')) {
+            return
+        }
+
+        swal({
+            text: `GitHub repo is not available. If you know this information, please contribute below:`,
+            type: 'warning',
+            showCancelButton: true,
+            input: 'text'
+        }).then(val => {
+            if (val.value) {
+                proposeNewData.call({
+                    projectId: this._id,
+                    datapoint: 'github_url',
+                    newData: val.value,
+                    type: 'link'
+                }, (err, data) => {
+                    if (err) {
+                        notify(err.reason || err.message, 'error')
+                    } else {
+                        notify('Successfully contributed.', 'success')
+                    }
+                })
+            }
+        })
+    },
+    'click .website': function(event, temlateInstance) {
+        if ($(event.currentTarget).attr('href')) {
+            return
+        }
+
+        swal({
+            text: `Website is not available. If you know this information, please contribute below:`,
+            type: 'warning',
+            showCancelButton: true,
+            input: 'text'
+        }).then(val => {
+            if (val.value) {
+                proposeNewData.call({
+                    projectId: this._id,
+                    datapoint: 'website',
+                    newData: val.value,
+                    type: 'link'
+                }, (err, data) => {
+                    if (err) {
+                        notify(err.reason || err.message, 'error')
+                    } else {
+                        notify('Successfully contributed.', 'success')
+                    }
+                })
+            }
+        })
+    },
+    'click .projectWarning' (event, _tpl) {
+        event.preventDefault()
+        console.log('here')
+        swal({
+            title: 'Missing source repository',
+            text: "This project does't contain any link to the source repository",
+            type: 'warning',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Okay'
+        })
+    }
 })
