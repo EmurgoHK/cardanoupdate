4cdaddf21844bf732e350c2e8bc62050f723b38b
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2018-11-01 21:04:07 +0100

    Problem: weird titles shown in tag search #389
    Solution: Added conditionals to show titles

--- imports/ui/pages/tag/tag.js
+++ imports/ui/pages/tag/tag.js
@@ -19,6 +19,11 @@ Template.tags.onCreated(function() {
     .filter(a => a.length > 0);
   this.searchFilter = new ReactiveVar(inputTags);
 
+  this.projects = new ReactiveVar();
+  this.warnings = new ReactiveVar();
+  this.learn = new ReactiveVar();
+  this.socialResources = new ReactiveVar();
+
   this.autorun(() => {
     this.subscribe("projects");
     this.subscribe("warnings");
@@ -33,72 +38,90 @@ Template.tags.onRendered(function() {
   let searchTags = tpl.searchFilter.get();
   
   tpl.$("#searchBox").val(searchTags.join(', '));
+  this.autorun(() => {
+    let searchTags = Template.instance().searchFilter.get();
+
+    // Check if user has searched for something
+    if (searchTags && searchTags.length > 0) {
+      this.projects.set(Projects.find({
+        $and: searchTags.map(tagName => ({
+          tags: {
+            $elemMatch: { name: tagName }
+          }
+        }))
+      }));
+
+      this.warnings.set(Warnings.find({
+        $and: searchTags.map(tagName => ({
+          tags: {
+            $elemMatch: { name: tagName }
+          }
+        }))
+      }));
+
+      this.learn.set(Learn.find({
+        $and: searchTags.map(tagName => ({
+          tags: {
+            $elemMatch: { name: tagName }
+          }
+        }))
+      }));
+
+      this.socialResources.set(socialResources.find({
+        $and: searchTags.map(tagName => ({
+          tags: {
+            $elemMatch: { name: tagName }
+          }
+        }))
+      }));
+    } else {
+      this.projects.set(Projects.find({}));
+      this.warnings.set(Warnings.find({}));
+      this.learn.set(Learn.find({}));
+      this.socialResources.set(socialResources.find({}));
+    }
+  });
 });
 
 Template.tags.helpers({
   projects() {
-    let searchTags = Template.instance().searchFilter.get();
-
-    // Check if user has searched for something
-    if (searchTags && searchTags.length > 0) {
-      return Projects.find({
-        $and: searchTags.map(tagName => ({
-          tags: {
-            $elemMatch: { name: tagName }
-          }
-        }))
-      });
-    } else {
-      return Projects.find({});
-    }
+    return Template.instance().projects.get();
+  },
+  hasProjects() {
+    const projects = Template.instance().projects.get();
+    return projects && projects.count() > 0;
   },
   warnings() {
-    let searchTags = Template.instance().searchFilter.get();
-
-    // Check if user has searched for something
-    if (searchTags && searchTags.length > 0) {
-      return Warnings.find({
-        $and: searchTags.map(tagName => ({
-          tags: {
-            $elemMatch: { name: tagName }
-          }
-        }))
-      });
-    } else {
-      return Warnings.find({});
-    }
+    return Template.instance().warnings.get();
+  },
+  hasWarnings() {
+    const warnings = Template.instance().warnings.get();
+    return warnings && warnings.count() > 0;
   },
   learn() {
-    let searchTags = Template.instance().searchFilter.get();
-
-    // Check if user has searched for something
-    if (searchTags && searchTags.length > 0) {
-      return Learn.find({
-        $and: searchTags.map(tagName => ({
-          tags: {
-            $elemMatch: { name: tagName }
-          }
-        }))
-      });
-    } else {
-      return Learn.find({});
-    }
+    return Template.instance().learn.get();
+  },
+  hasLearn() {
+    const learn = Template.instance().learn.get();
+    return learn && learn.count() > 0;
   },
   socialResources() {
-    let searchTags = Template.instance().searchFilter.get();
+    return Template.instance().socialResources.get();
+  },
+  hasSocialResources() {
+    const socialResources = Template.instance().socialResources.get();
+    return socialResources && socialResources.count() > 0;
+  },
+  hasAny() {
+    const projects = Template.instance().projects.get();
+    const warnings = Template.instance().warnings.get();
+    const learn = Template.instance().learn.get();
+    const socialResources = Template.instance().socialResources.get();
 
-    // Check if user has searched for something
-    if (searchTags && searchTags.length > 0) {
-      return socialResources.find({
-        $and: searchTags.map(tagName => ({
-          tags: {
-            $elemMatch: { name: tagName }
-          }
-        }))
-      });
-    } else {
-      return socialResources.find({});
-    }
+    return (projects && projects.count() > 0) ||
+           (warnings && warnings.count() > 0) ||
+           (learn && learn.count() > 0) ||
+           (socialResources && socialResources.count() > 0);
   }
 });
 
