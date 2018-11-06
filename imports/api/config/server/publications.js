0085de1b2af97c5fbfecd11363163aaaf4416dce
Andrej Budinčević <andrew@hotmail.rs>
2018-11-06 21:54:27 +0100

    Problem: Google maps API credentials not fetched from mongo (#465)
    Solution: Load the key for Google Maps API from `Config` collection. Only load the Google Maps API script on the events form page to prevent unnecessary loading. Fix a small bug with breadcrumbs.

--- imports/api/config/server/publications.js
+++ imports/api/config/server/publications.js
@@ -1,4 +1,8 @@
 import { Meteor } from 'meteor/meteor'
 import { Config } from '../config'
 
-Meteor.publish('config', () => Config.findOne({}))
+Meteor.publish('config', () => Config.find({
+	_id: {
+		$in: ['google-maps-api'] // add _id's that are safe to be published on client here
+	}
+}))
