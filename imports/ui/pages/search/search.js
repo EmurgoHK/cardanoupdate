96d9facbdd69749036ceea053a34c5672be5e6da
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2018-11-08 10:46:24 +0100

    Problem: search resets filters on refresh #475
    Solution: Updated how we filter results, stored filters in queryparam, updated UI

--- imports/ui/pages/search/search.js
+++ imports/ui/pages/search/search.js
@@ -11,27 +11,39 @@ import { Warnings } from '/imports/api/warnings/warnings'
 import './search.scss'
 
 Template.search.onCreated(function() {
+    this.filters= new ReactiveVar([]);
     this.autorun(() => {
-
         let q = FlowRouter.getQueryParam('q');
 
+        const typeParam = FlowRouter.getQueryParam('type');
+        let filters =  typeParam !== undefined ? typeParam.split('-') : [
+            'projects',
+            'events',
+            'learn',
+            'research',
+            'warnings',
+            'socialResources',
+        ];
+
         this.subscribe('projects.search', q)
-        this.subscribe('events.search', q)
-        this.subscribe('learn.search', q)
-        this.subscribe('research.search', q)
-        this.subscribe('warnings.search', q)
-        this.subscribe('socialResources.search', q)
+        this.subscribe('events.search', q);
+        this.subscribe('learn.search', q);
+        this.subscribe('research.search', q);
+        this.subscribe('warnings.search', q);
+        this.subscribe('socialResources.search', q);
+       
+        this.filters.set(filters);
     })
 })
 
 Template.search.helpers({
     recordsFound() {
-        let events = Events.find({}).count();
-        let projects = Projects.find({}).count();
-        let learn = Learn.find({}).count();
-        let research = Research.find({}).count();
-        let socialResourcesCount = socialResources.find({}).count();
-        let warnings = Warnings.find({}).count();
+        let events = Template.instance().filters.get().indexOf('events') !== -1 ? Events.find({}).count() : 0;
+        let projects = Template.instance().filters.get().indexOf('projects') !== -1 ? Projects.find({}).count() : 0;
+        let learn = Template.instance().filters.get().indexOf('learn') !== -1 ? Learn.find({}).count() : 0;
+        let research = Template.instance().filters.get().indexOf('research') !== -1 ? Research.find({}).count() : 0;
+        let socialResourcesCount = Template.instance().filters.get().indexOf('socialResources') !== -1 ? socialResources.find({}).count() : 0;
+        let warnings = Template.instance().filters.get().indexOf('warnings') !== -1 ? Warnings.find({}).count() : 0;
 
         return events + projects + learn + research + socialResourcesCount + warnings;
     },
@@ -53,6 +65,24 @@ Template.search.helpers({
     warnings() {
         return Warnings.find({})
     },
+    showProjects: () => Template.instance().filters.get().indexOf('projects') !== -1,
+    projectsContainerClass: () => Template.instance().filters.get().indexOf('projects') !== -1 ? 'resultContainer' : 'shrinkToHidden',
+    
+    showEvents: () => Template.instance().filters.get().indexOf('events') !== -1,
+    eventsContainerClass: () => Template.instance().filters.get().indexOf('events') !== -1 ? 'resultContainer' : 'shrinkToHidden',
+    
+    showLearn: () => Template.instance().filters.get().indexOf('learn') !== -1,
+    learnContainerClass: () => Template.instance().filters.get().indexOf('learn') !== -1 ? 'resultContainer' : 'shrinkToHidden',
+    
+    showResearch: () => Template.instance().filters.get().indexOf('research') !== -1,
+    researchContainerClass: () => Template.instance().filters.get().indexOf('research') !== -1 ? 'resultContainer' : 'shrinkToHidden',
+    
+    showWarnings: () => Template.instance().filters.get().indexOf('warnings') !== -1,
+    warningsContainerClass: () => Template.instance().filters.get().indexOf('warnings') !== -1 ? 'resultContainer' : 'shrinkToHidden',
+    
+    showSocialResources: () => Template.instance().filters.get().indexOf('socialResources') !== -1,
+    socialResourcesContainerClass: () => Template.instance().filters.get().indexOf('socialResources') !== -1 ? 'resultContainer' : 'shrinkToHidden',
+
     query() {
         return FlowRouter.getQueryParam('q')
     },
@@ -61,23 +91,23 @@ Template.search.helpers({
 })
 
 Template.search.events({
-    'click #projectCheckbox': (event, _) => {
-        $(".projectsContainer").fadeToggle();
+    'click #projectCheckbox': (event, templateInstance) => {
+        toggleFilter(templateInstance, 'projects');
     },
-    'click #eventsCheckbox': (event, _) => {
-        $(".eventsContainer").fadeToggle();
+    'click #eventsCheckbox': (event, templateInstance) => {
+        toggleFilter(templateInstance, 'events');
     },
-    'click #learnCheckbox': (event, _) => {
-        $(".learnContainer").fadeToggle();
+    'click #learnCheckbox': (event, templateInstance) => {
+        toggleFilter(templateInstance, 'learn');
     },
-    'click #researchCheckbox': (event, _) => {
-        $(".researchContainer").fadeToggle();
+    'click #researchCheckbox': (event, templateInstance) => {
+        toggleFilter(templateInstance, 'research');
     },
-    'click #scamsCheckbox': (event, _) => {
-        $(".scamsContainer").fadeToggle();
+    'click #scamsCheckbox': (event, templateInstance) => {
+        toggleFilter(templateInstance, 'warnings');
     },
-    'click #communityCheckbox': (event, _) => {
-        $(".communityContainer").fadeToggle();
+    'click #communityCheckbox': (event, templateInstance) => {
+        toggleFilter(templateInstance, 'socialResources');
     },
     'click #searchButton, submit': (event, templateInstance) => {
         event.preventDefault();
@@ -93,3 +123,20 @@ Template.search.events({
 
     },
 })
+
+function toggleFilter(templateInstance, filter) {
+    const filters = templateInstance.filters.get();
+    const index = filters.indexOf(filter);
+    
+    // We push filter in if it wasn't present and remove it if it was
+    if (index === -1)
+        filters.push(filter)
+    else 
+        filters.splice(index, 1);
+    
+    templateInstance.filters.set(filters);
+
+    // We save to a queryparam to preserve it after refresh
+    // It's sorted to have consistent urls for filters
+    FlowRouter.setQueryParams({'type': filters.sort().join('-')});
+}
