ebe6b0b51cd05d4bbcb4c6768271b008d394dbbe
Andrej Budinčević <andrew@hotmail.rs>
2018-12-03 22:03:54 +0100

    Problem: Users don't know how to add new languages (#625)
    Solution: Add an 'add new language' field to the language selector in the header and in the footer.

--- imports/ui/shared/footer/footer.js
+++ imports/ui/shared/footer/footer.js
@@ -3,19 +3,26 @@ import "./footer.scss";
 
 Template.footer.helpers({
   languages: () => {
-    return Object.keys(TAPi18n.languages_names).map(key => {
-      return {
-        code: key,
-        name: TAPi18n.languages_names[key][1],
-        selected: key === TAPi18n.getLanguage()
-      };
-    });
+    return _.union(Object.keys(TAPi18n.languages_names).map(key => {
+        return {
+          code: key,
+          name: TAPi18n.languages_names[key][1],
+          selected: key === TAPi18n.getLanguage()
+        };
+      }), [{
+        code: 'new',
+        name: TAPi18n.__('shared.add_language')
+      }]);
   }
 });
 
 Template.footer.events({
   "change #selectLanguage"(event) {
     event.preventDefault();
-    TAPi18n.setLanguage(event.target.value);
+    if (event.target.value === 'new') {
+      FlowRouter.go('/translations')
+    } else {
+      TAPi18n.setLanguage(event.target.value);
+    }
   }
 });
