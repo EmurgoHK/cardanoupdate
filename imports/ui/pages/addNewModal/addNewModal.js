1c27db026b15972299aa91a1ff364297cdc2acd2
Ajay Katrodiya <ajaykatrodiya@gmail.com>
2019-02-15 11:31:41 +0530

    Problem: [Add Content] if not logged in, it open add popup #733
    Solution: open modal for login and after login redirect to add form

--- imports/ui/pages/addNewModal/addNewModal.js
+++ imports/ui/pages/addNewModal/addNewModal.js
@@ -4,6 +4,13 @@ import './addNewModal.scss'
 Template.AddNewModal.events({
   'click .list-group-item': (event, _) => {
     $('#newModal').modal('hide')
+  },
+  'click .list-group-item a' (event, template) {
+    if (!Meteor.userId()) {
+      event.preventDefault()
+      const from = $(event.currentTarget).attr('href')
+      $('#loginModal').modal('show')
+      FlowRouter.setQueryParams({from})
+    }
   }
-
-  })
+})
