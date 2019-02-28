b52dc3e812f8223ff704e7c2c43d753efabc2a7a
Ajay Katrodiya <ajaykatrodiya@gmail.com>
2019-02-28 19:44:18 +0530

    Problem: [Report] report ask login, instead of open login popup #740
    Solution: It should open login popup

--- imports/ui/helpers/loggedInSWAL.js
+++ imports/ui/helpers/loggedInSWAL.js
@@ -1,20 +1,24 @@
 import swal from 'sweetalert2'
 
-export async function loginSWAL(opts) {
-  const result = await swal({
-    type: 'warning',
-    title: TAPi18n.__('shared.loginModal.title', {action: TAPi18n.__(opts.action, 'shared.loginModal.defaultAction')}),
-    confirmButtonText: TAPi18n.__('shared.loginModal.login'),
-    cancelButtonText: TAPi18n.__('shared.loginModal.cancel'),
-    showCancelButton: true,
-  });
-  
-  if (result.value) {
-    FlowRouter.go('/login?from=' + FlowRouter.current().path);
-    return {value: false};
-  } else {
-    return result;
-  }
+export async function loginSWAL(opts, callback) {
+  $('#loginModal').modal('show')
+  // const modalFn = Meteor.wrapAsync(getModalCallback)
+  // return await modalFn()
+  return { value: false }
+  // const result = await swal({
+  //   type: 'warning',
+  //   title: TAPi18n.__('shared.loginModal.title', {action: TAPi18n.__(opts.action, 'shared.loginModal.defaultAction')}),
+  //   confirmButtonText: TAPi18n.__('shared.loginModal.login'),
+  //   cancelButtonText: TAPi18n.__('shared.loginModal.cancel'),
+  //   showCancelButton: true,
+  // });
+  //
+  // if (result.value) {
+  //   FlowRouter.go('/login?from=' + FlowRouter.current().path);
+  //   return {value: false};
+  // } else {
+  //   return result;
+  // }
 }
 
 export async function loggedInSWAL(swalOpts) {
@@ -24,3 +28,9 @@ export async function loggedInSWAL(swalOpts) {
     return swal(swalOpts);
   }
 }
+
+// const getModalCallback = function (callback) {
+//   $('#loginModal').on('hide.bs.modal', () => {
+//     callback && callback(null, {value: true})
+//   })
+// }
